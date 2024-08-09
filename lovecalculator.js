6a8d66e9fc39ae9b52e92430d14f8e77721ecaf8
Yashaswini KB <yashashwinikb1999@gmail.com>
2024-08-09 20:18:55 +0530

    alert

--- lovecalculator.js
+++ lovecalculator.js
@@ -4,3 +4,4 @@ promt("What is your name");
 var x = Math.random(); //generats num from 0 to 0.999, 1 not included
 lovescore = Math.floor(x) + 1 //floor rounds the floating num
 console.log(lovescore)
+alert("Your lovescore is" + lovescore)

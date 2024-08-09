7ba6b6cd1b8b7c6c3e5939d29d274cff14861521
Yashaswini KB <yashashwinikb1999@gmail.com>
2024-08-09 22:18:45 +0530

    DOM

--- advanced-js.js
+++ advanced-js.js
@@ -7,14 +7,56 @@
 //     alert("Sorry, next time.")
 // }
 
+
+//fizzbuzz
+// var output = [];
+// var count = 1
+
+// function fizzbuzz(){
+// while(count <= 100){
+// if (count % 3 === 0 && count % 5 === 0 ){
+//     output.push("FizzBuzz");
+// }
+// else if (count % 3 === 0 ){
+//     output.push("Fizz");
+// }
+// else if (count % 5 === 0){
+//     output.push("Buzz");
+// }
+// else {
+//     output.push(count)
+// }
+// }
+// console.log(output)
+// }
+    
+//with for loop
+// function fizzbuzz(){
+//     for (count =1; count < 101; count++) {
+//     if (count % 3 === 0 && count % 5 === 0 ){
+//         output.push("FizzBuzz");
+//     }
+//     else if (count % 3 === 0 ){
+//         output.push("Fizz");
+//     }
+//     else if (count % 5 === 0){
+//         output.push("Buzz");
+//     }
+//     else {
+//         output.push(count)
+//     }
+//     }
+//     console.log(output)
+// }
+
 //who is buying lunch
-var guestlist = ["Angela", "Jack", "Pam"]
-function whoispaying(guestlist){
-    var num = guestlist.length;
-    var loc = Math.floor(Math.random() * num);
-    var randomPerson = guestlist[loc];
-    return randomPerson + " is buying lunch!"
-}
+// var guestlist = ["Angela", "Jack", "Pam"]
+// function whoispaying(guestlist){
+//     var num = guestlist.length;
+//     var loc = Math.floor(Math.random() * num);
+//     var randomPerson = guestlist[loc];
+//     return randomPerson + " is buying lunch!"
+// }
 
 //beer song
 // var numberOfBottles = 99
@@ -29,6 +71,5 @@ function whoispaying(guestlist){
 // 	numberOfBottles--;
 //     console.log(numberOfBottles + " " + bottleWord + " of beer on the wall.");
 // }
-
 
 

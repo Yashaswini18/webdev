bef0cd8644101c80fb427dbd4ae587ae3a62cefe
Yashaswini KB <yashashwinikb1999@gmail.com>
2024-08-09 20:15:04 +0530

    Love calculator game

--- lovecalculator.js
+++ lovecalculator.js
@@ -1,3 +1,6 @@
-x = Math.random() * 100;
-lovescore = Math.floor(x) + 1
+promt("What is your name");
+promt("What is your name");
+
+var x = Math.random(); //generats num from 0 to 0.999, 1 not included
+lovescore = Math.floor(x) + 1 //floor rounds the floating num
 console.log(lovescore)
